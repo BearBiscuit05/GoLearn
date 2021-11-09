68b99c3e92660ae61fbe46576baf8d5beb1d10f1
BearBiscuit <742035327@qq.com>
2021-11-09 16:30:16 +0800

    user online broadcast

--- server.go
+++ server.go
@@ -3,24 +3,66 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+
+	//online map table
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	Message chan string
 }
 
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 
 	return server
 }
 
+func (this *Server) ListenMessager() {
+	fmt.Println("Listen init success")
+	for {
+		fmt.Println("Listening ")
+		msg := <-this.Message
+
+		//发送消息
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+
+		this.mapLock.Unlock()
+	}
+}
+
+func (this *Server) BroadCast(user *User, msg string) {
+
+	fmt.Println("broad func success")
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+
+	this.Message <- sendMsg
+
+}
+
 func (this *Server) Handler(conn net.Conn) {
 	fmt.Println("connction success")
+
+	user := NewUser(conn)
+	//将用户上线广播
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+
+	this.BroadCast(user, "online")
 }
 
 func (this *Server) Start() {
@@ -32,6 +74,8 @@ func (this *Server) Start() {
 	}
 	//close listen socket
 	defer listener.Close()
+
+	go this.ListenMessager()
 
 	for {
 		//accept
